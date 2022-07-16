1885fa4e4a5bc8abb15a5c5b17d60c645fabe67d
Ian <s1ic3dbr3ad@gmail.com>
2022-07-15 19:29:08 -0500

    ini

---
+++ SpellSchoolCounter/SpellSchoolCounter/SpellSchoolCounter.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Hearthstone_Deck_Tracker;
+using Hearthstone_Deck_Tracker.Enums;
+using Hearthstone_Deck_Tracker.Hearthstone;
+using Hearthstone_Deck_Tracker.Hearthstone.Entities;
+using CoreAPI = Hearthstone_Deck_Tracker.API.Core;
+
+namespace SpellSchoolCounter
+{
+    public class SpellSchoolCounter
+    {
+        private SchoolCountWidget _playerList = null;
+        private SchoolCountWidget _opponentList = null;
+
+        public SpellSchoolCounter(SchoolCountWidget playerList, SchoolCountWidget opponentList)
+        {
+
+            _playerList = playerList;
+            _opponentList = opponentList;
+
+            // Hide in menu, if necessary
+            if (Config.Instance.HideInMenu && CoreAPI.Game.IsInMenu)
+            {
+                _playerList.Hide();
+                _opponentList.Hide();
+            }
+				
+        }
+
+        // Reset on when a new game starts
+        internal void GameStart()
+        {
+            _playerList.Update(new List<Card>());
+            _opponentList.Update(new List<Card>());
+        }
+
+        internal void OnPlayerPlay(Card card)
+        {
+            if (!string.IsNullOrEmpty(card.EnglishText) && card.EnglishText.Contains("Battlecry:") && ! card.EnglishText.Contains("Repeat the last Battlecry"))
+            {
+                _playerList.Update(new List<Card>() { card });
+            }
+        }
+
+        internal void OnOpponentPlay(Card card)
+        {
+            if (!string.IsNullOrEmpty(card.EnglishText) && card.EnglishText.Contains("Battlecry:") && !card.EnglishText.Contains("Repeat the last Battlecry"))
+            { 
+                _opponentList.Update(new List<Card>() { card });
+            }
+        }
+
+        // Need to handle hiding the element when in the game menu
+        internal void InMenu()
+        {
+            if (Config.Instance.HideInMenu)
+            {
+                _playerList.Hide();
+                _opponentList.Hide();
+            }
+        }
+
+        // Update the card list on player's turn
+        internal void TurnStart(ActivePlayer player)
+        {
+
+            _playerList.Show();
+            _opponentList.Show();
+        }
+		
+    }
+}
---
+++ SpellSchoolCounter/SpellSchoolCounter/SpellSchoolCounterPlugin.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Controls;
+using Hearthstone_Deck_Tracker.API;
+using Hearthstone_Deck_Tracker.Plugins;
+
+namespace SpellSchoolCounter
+{
+    public class SpellSchoolCounterPlugin : IPlugin
+    {
+        private SchoolCountWidget _playerList;
+        private SchoolCountWidget _opponentList;
+
+        public string Author
+        {
+            get { return "Slicedbread"; }
+        }
+
+        public string ButtonText
+        {
+            get { return "Settings"; }
+        }
+
+        public string Description
+        {
+            get { return "Counts number of different spell schools you've played for cards like multicaster"; }
+        }
+
+        public MenuItem MenuItem
+        {
+            get { return null; }
+        }
+
+        public string Name
+        {
+            get { return "Spell School Counter"; }
+        }
+
+        public void OnButtonPress()
+        {
+        }
+
+        public void OnLoad()
+        {
+            _playerList = new SchoolCountWidget(Location.Player);
+            _opponentList = new SchoolCountWidget(Location.Opponent);
+			
+            Core.OverlayCanvas.Children.Add(_playerList);
+            Core.OverlayCanvas.Children.Add(_opponentList);
+
+            SpellSchoolCounter curvy = new SpellSchoolCounter(_playerList, _opponentList);
+
+
+            GameEvents.OnGameStart.Add(curvy.GameStart);
+            GameEvents.OnInMenu.Add(curvy.InMenu);
+            GameEvents.OnTurnStart.Add(curvy.TurnStart);
+            GameEvents.OnPlayerPlay.Add(curvy.OnPlayerPlay);
+            GameEvents.OnOpponentPlay.Add(curvy.OnOpponentPlay);
+
+
+        }
+
+        public void OnUnload()
+        {
+            Core.OverlayCanvas.Children.Remove(_playerList);
+            Core.OverlayCanvas.Children.Remove(_opponentList);
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public Version Version
+        {
+            get { return new Version(1,0,0); }
+        }
+    }
+}
